08215179bbcae23b60bcccc919bb95a349862951
Christopher Davis <chrisd@gravitybox.com>
2018-02-14 19:54:30 -0500

    reset populations for ZIP codes

--- ModelProject/Program.cs
+++ ModelProject/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CsvHelper;
+using Gravitybox.GeoLocation.EFDAL;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +12,44 @@ namespace ModelProject
     {
         static void Main(string[] args)
         {
+            //ResetPopulations();
         }
+
+        private static void ResetPopulations()
+        {
+            var index = 0;
+            var connectionString = @"server=.\SQL2014;initial catalog=GeoLocation;integrated security=SSPI;";
+            using (var context = new GeoLocationEntities(connectionString))
+            {
+                var allZips = context.Zip.ToList();
+                using (var sr = new StreamReader(@"C:\Users\chrisd\Downloads\Zipcode-ZCTA-Population-Density-And-Area-Unsorted.csv"))
+                {
+                    using (var csv = new CsvReader(sr))
+                    {
+                        while (csv.Read())
+                        {
+                            var record = csv.GetRecord<ZipItem>();
+                            if (record.Population > 0)
+                            {
+                                var dbZip = allZips.FirstOrDefault(x => x.Name == record.Zip);
+                                if (dbZip != null)
+                                {
+                                    Console.WriteLine($"Index={index}, Zip={record.Zip}, Population1={record.Population}, Population2={dbZip.Population}");
+                                    dbZip.Population = record.Population;
+                                }
+                            }
+                            index++;
+                        }
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+
+    internal class ZipItem
+    {
+        public string Zip { get; set; }
+        public int Population { get; set; }
     }
 }
